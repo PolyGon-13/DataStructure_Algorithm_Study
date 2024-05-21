07d00b523c92b47ae9b5654e2dbb7a7544bd1277
Donghyun Kim <107293272+PolyGon-13@users.noreply.github.com>
2024-05-21 23:10:15 +0900

    Update Q-1.c

--- C/question/Q-1.c
+++ C/question/Q-1.c
@@ -1,1 +1,17 @@
+#include <stdio.h>
 
+int max4(int a,int b,int c, int d)
+{
+	int max=a;
+	if(b>max) max=b;
+	if(c>max) max=c;
+	if(d>max) max=d;
+	return max;
+}
+
+int main()
+{
+	printf("max4(%d, %d, %d %d)=%d\n",3,2,1,4,max4(3,2,1,4));
+	
+	return 0;
+}
