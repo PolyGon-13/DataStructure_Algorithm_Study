10f3349b9b6b89fdfb43a9db63dd1d7b1dbc2c0a
Donghyun Kim <107293272+PolyGon-13@users.noreply.github.com>
2024-05-31 22:58:52 +0900

    Update 3-4_bin_search.c

--- C/practice/03/3-4_bin_search.c
+++ C/practice/03/3-4_bin_search.c
@@ -3,21 +3,22 @@
 
 int bin_search(const int a[],int n,int key)
 {
-	int pl=0;
-	int pr=n-1;
+	int pl=0; // 검색 범위 맨 앞의 인덱스
+	int pr=n-1; // 검색 범위 맨 끝의 인덱스
 	
 	do
 	{
 		int pc=(pl+pr)/2;
-		if(a[pc]==key)
-			return pc;
+		
+		if(a[pc]==key) // 검색 범위 가운데의 인덱스
+			return pc; // 검색 성공
 		else if(a[pc]<key)
-			pl=pc+1;
+			pl=pc+1; // 검색 범위를 뒤쪽 절반으로 좁힘
 		else
-			pr=pc-1;
-	} while(pl<=pr);
+			pr=pc-1; // 검색 범위를 앞쪽 절반으로 좁힘
+	} while(pl<=pr); // pl과 pr이 하나의 수로 좁혀질 때까지 찾는 값이 없는 경우 do문 탈출
 	
-	return -1;
+	return -1; // 검색 실패
 }
 
 int main()
