83c1d876c758a9883894046b974b3bbe7c9cd33b
Donghyun Kim <107293272+PolyGon-13@users.noreply.github.com>
2024-06-07 23:21:36 +0900

    Update 4C-1_last.c

--- C/practice/04/4C-1_last.c
+++ C/practice/04/4C-1_last.c
@@ -11,12 +11,12 @@ int main()
 	do
 	{
 		printf("%d번째 정수 : ",cnt+1);
-		scanf("%d",&a[cnt++%N]);
+		scanf("%d",&a[cnt++%N]); // 0~9까지 10개의 수를 저장한 뒤 더 저장하는 경우 cnt++%N을 통해 0번째 인덱스부터 다시 덮어씀 (링 버퍼)
 		printf("계속할까요?(Yes---1 / No---0) : ");
 		scanf("%d",&retry);
 	} while(retry==1);
 	
-	int i=cnt-N;
+	int i=cnt-N; // cnt에 N을 빼주어 가장 마지막에 입려된 N개의 수를 출력할 수 있도록 함
 	if(i<0) i=0;
 	for(;i<cnt;i++)
 		printf("%2d번째 정수 = %d\n",i+1,a[i%N]);
