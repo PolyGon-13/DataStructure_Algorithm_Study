cf5cda71ced0c12aaf89853572643ec886294d44
Donghyun Kim <107293272+PolyGon-13@users.noreply.github.com>
2024-06-11 23:27:33 +0900

    Create 5-5_recur_nr2.c

---
+++ C/practice/05/5-5_recur_nr2.c
@@ -0,0 +1,90 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct
+{
+	int max;
+	int ptr;
+	int* stk;
+} IntStack;
+
+int Initialize(IntStack* s,int max)
+{
+	s->ptr=0;
+	
+	if((s->stk=calloc(max,sizeof(int)))==NULL)
+	{
+		s->max=0;
+		return -1;
+	}
+	s->max=max;
+	
+	return 0;
+}
+
+int Push(IntStack* s,int x)
+{
+	if(s->ptr >= s->max)
+		return -1;
+	s->stk[s->ptr++]=x;
+	
+	return 0;
+}
+
+int Pop(IntStack* s,int* x)
+{
+	if(s->ptr<=0)
+		return -1;
+	*x=s->stk[--s->ptr];
+	
+	return 0;
+}
+
+int IsEmpty(const IntStack* s)
+{
+	return s->ptr<=0;
+}
+
+int IsFull(const IntStack* s)
+{
+	return s->ptr>=s->max;
+}
+
+void Terminate(IntStack* s)
+{
+	if(s->stk != NULL)
+		free(s->stk);
+	s->max=s->ptr=0;
+}
+
+void recur(int n)
+{
+	IntStack stk;
+	Initialize(&stk,100);
+	
+Top:
+	if(n>0)
+	{
+		Push(&stk,n);
+		n=n-1;
+		goto Top;
+	}
+	if(!IsEmpty(&stk))
+	{
+		Pop(&stk,&n);
+		printf("%d\n",n);
+		n=n-2;
+		goto Top;
+	}
+	Terminate(&stk);
+}
+
+int main()
+{
+	int x;
+	printf("정수를 입력하세요: ");
+	scanf("%d",&x);
+	recur(x);
+	
+	return 0;
+}
