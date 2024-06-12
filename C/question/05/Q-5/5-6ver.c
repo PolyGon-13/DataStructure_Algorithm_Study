cfa8f7b181acc2f9a03b3dc6f6cf42b95d2df86c
Donghyun Kim <107293272+PolyGon-13@users.noreply.github.com>
2024-06-12 22:33:33 +0900

    Create 5-6ver.c

---
+++ C/question/05/Q-5/5-6ver.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <string.h>
+
+static char memo[128][1024];
+int cnt=0;
+
+void recur(int n)
+{
+	if(memo[n+1][0]!='\0')
+		printf("%s",memo[n+1]);
+	else
+	{
+		if(n>0)
+		{
+			recur(n-1);
+			cnt++;
+			printf("%d\n",n);
+			recur(n-2);
+			cnt++;
+			sprintf(memo[n+1],"%s%d\n%s",memo[n],n,memo[n-1]);
+		}
+		else
+			strcpy(memo[n+1],"");
+	}
+}
+
+int main()
+{
+	int x;
+	printf("정수를 입력하세요: ");
+	scanf("%d",&x);
+	
+	cnt++;
+	recur(x);
+	printf("\n%d\n",cnt);
+	
+	return 0;
+}
