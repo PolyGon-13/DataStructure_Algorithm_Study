61c82d9eeb016d434bd69fc51454edf4e46bef11
Donghyun Kim <107293272+PolyGon-13@users.noreply.github.com>
2024-06-13 23:34:52 +0900

    Update and rename C/question/05/Q-5/5-6ver.c to C/question/05/Q-5.c

--- C/question/05/Q-5.c
+++ C/question/05/Q-5.c
@@ -1,3 +1,35 @@
+// 5-3ver
+#include <stdio.h>
+
+static int cnt;
+
+void recur(int n)
+{
+	if(n>0)
+	{
+		recur(n-1);
+		cnt++;
+		printf("%d\n",n);
+		recur(n-2);
+		cnt++;
+	}
+}
+
+int main()
+{
+	int x;
+	printf("정수를 입력하세요: ");
+	scanf("%d",&x);
+	
+	cnt++;
+	recur(x);
+	printf("\n%d\n",cnt);
+	
+	return 0;
+}
+
+
+// 5-6ver
 #include <stdio.h>
 #include <string.h>
 
