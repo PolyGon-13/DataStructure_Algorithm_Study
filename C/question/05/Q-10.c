f6df64dfd61df39c166165034b2e746d398325d6
Donghyun Kim <107293272+PolyGon-13@users.noreply.github.com>
2024-06-16 23:53:41 +0900

    Update Q-10.c

--- C/question/05/Q-10.c
+++ C/question/05/Q-10.c
@@ -76,35 +76,38 @@ void set(int i)
 	Initialize(&stk, 8);
 
 Start:
-	while (1) 
+	while(1) 
 	{
-		j = 0;
-		while (1) 
+		j=0;
+		while(1)
 		{
-			while (j < 8) 
+			while(j<8) 
 			{
-				if (!flag_a[j] && !flag_b[i + j] && !flag_c[i - j + 7]) 
+				if(!flag_a[j] && !flag_b[i+j] && !flag_c[i-j+7]) 
 				{
-					pos[i] = j;
-					if (i == 7) // 전체 열 배치 마침
+					pos[i]=j;
+					if(i==7) // 전체 열 배치 마침
 						print();
 					else 
 					{
-						flag_a[j] = flag_b[i + j] = flag_c[i - j + 7] = 1;
+						flag_a[j]=flag_b[i+j]=flag_c[i-j+7]=1;
 						i++;
-						Push(&stk, j); // i번째 열의 행을 푸시
+						Push(&stk,j); // i번째 열의 행을 푸시
 						goto Start;
 					}
 				}
 				j++;
 			}
-			if (--i == -1) 
+			if(--i==-1)
 				return;
-			Pop(&stk, &j); // i번째 열의 행을 팝
-			flag_a[j] = flag_b[i + j] = flag_c[i - j + 7] = 0;
+			Pop(&stk,&j); // i번째 열의 행을 팝
+			flag_a[j]=flag_b[i+j]=flag_c[i-j+7]=0;
 			j++;
 		}
+		// 하나의 케이스를 우선 구성하고, 배치된 행의 열 번호를 저장해둔다.
+		// 8열까지 모두 진행 후, if문에 의해 i가 7로 내려오고, 이전에 넣어둔 7열의 퀸을 배치한 번호 j를 pop하여 다른 경우의 수를 찾는다.
 	}
+	
 	Terminate(&stk);
 }
 
