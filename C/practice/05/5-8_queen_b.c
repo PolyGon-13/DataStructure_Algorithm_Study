def4fa945f7c5000fe3f49f3b5d250fe2e9c6574
Donghyun Kim <107293272+PolyGon-13@users.noreply.github.com>
2024-06-16 22:52:17 +0900

    Create 5-8_queen_b.c

---
+++ C/practice/05/5-8_queen_b.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+int pos[8];
+
+void print(void)
+{
+	int i;
+	for(i=0;i<8;i++)
+		printf("%2d",pos[i]);
+	putchar('\n');
+}
+
+void set(int i)
+{
+	int j;
+	for(j=0;j<8;j++)
+	{
+		pos[i]=j;
+		if(i==7)
+			print();
+		else
+			set(i+1);
+	}
+	// set(0)부터 set(7)까지 j=0일때 쭉 실행되어 set(7)에서 모든 요소에 0이 담긴채로 한 번 출력
+	// set(7)이 마저 실행됨. j가 1부터 7까지.
+	// 그리고 이제 set(6)으로 내려옴. j가 바뀌어 매 실행마다 다시 set(7)이 실행되어 pos[6]과 pos[7]의 값이 계속 바뀜.
+}
+
+int main()
+{
+	set(0);
+	
+	return 0;
+}
