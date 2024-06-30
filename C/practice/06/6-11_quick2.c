22a19270338ebc5751cbc611451dab62ffbbcaf9
Donghyun Kim <hyundiego@hanyang.ac.kr>
2024-06-30 22:44:43 +0900

    Create 6-11_quick2.c

---
+++ C/practice/06/6-11_quick2.c
@@ -0,0 +1,65 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define swap(type,x,y) do{type t=x; x=y; y=t;} while(0)
+
+int sort3elem(int x[],int a,int b,int c)
+{
+	if(x[b]<x[a]) swap(int,x[b],x[a]);
+	if(x[c]<x[b]) swap(int,x[c],x[b]);
+	if(x[b]<x[a]) swap(int,x[b],x[a]);
+	return b;
+}
+
+void quick(int a[],int left,int right)
+{
+	int pl=left;
+	int pr=right;
+	int m=sort3elem(a,pl,(pl+pr)/2,pr);
+	int x=a[m];
+	
+	swap(int,a[m],a[right-1]);
+	pl++;
+	pr-=2;
+	
+	do
+	{
+		while(a[pl]<x) pl++;
+		while(a[pr]>x) pr--;
+		if(pl<=pr)
+		{
+			swap(int,a[pl],a[pr]);
+			pl++;
+			pr--;
+		}
+	} while(pl<=pr);
+	
+	if(left<pr) quick(a,left,pr);
+	if(pl<right) quick(a,pl,right);
+}
+
+int main()
+{
+	int i;
+	int nx;
+	puts("퀵 정렬");
+	printf("요소 개수 : ");
+	scanf("%d",&nx);
+	int *x=calloc(nx,sizeof(int));
+	
+	for(i=0;i<nx;i++)
+	{
+		printf("x[%d] : ",i);
+		scanf("%d",&x[i]);
+	}
+	
+	quick(x,0,nx-1);
+	
+	puts("오름차순으로 정렬했습니다.");
+	for(i=0;i<nx;i++)
+		printf("x[%d] = %d\n",i,x[i]);
+	
+	free(x);
+	
+	return 0;
+}
