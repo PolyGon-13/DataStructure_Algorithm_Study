fbdf8d51d614347c5bc189488a3ba54feacdfe06
Donghyun Kim <hyundiego@hanyang.ac.kr>
2024-07-03 22:18:57 +0900

    Create Q-18.c

---
+++ C/question/06/Q-18.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+void q_sort(void *base,size_t nmemb,size_t size,int(*compar)(const void *,const void *))
+{
+	
+}
+
+int main()
+{
+	
+}
