051d1745671acd589341593f66189bf931d6932f
Donghyun Kim <hyundiego@hanyang.ac.kr>
2024-07-04 23:03:08 +0900

    Create Q-22.c

---
+++ C/question/06/Q-22.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void counting(int a[],int n,int min,int max)
+{
+	int i;
+	int *f=calloc(max-min+2,sizeof(int));
+	int *b=calloc(n,sizeof(int));
+	
+	for(i=0;i<=max-min+1;i++) f[i]=0;
+	for(i=0;i<n;i++) f[a[i]-min]++;
+	for(i=1;i<=max-min+1;i++) f[i]+=f[i-1];
+	for(i=n-1;i>=0;i--) b[--f[a[i]-min]]=a[i];
+	for(i=0;i<n;i++) a[i]=b[i];
+	
+	free(b);
+	free(f);
+}
+
+int main()
+{
+	int i;
+	int nx;
+	int min,max;
+	puts("도수 정렬");
+	printf("요소 개수 : ");
+	
+	scanf("%d",&nx);
+	int *x=calloc(nx,sizeof(int));
+	scanf("%d %d",&min,&max);
+	
+	printf("0~%d의 정수를 입력하세요\n",max);
+	for(i=0;i<nx;i++)
+	{
+		do
+		{
+			printf("x[%d] : ",i);
+			scanf("%d",&x[i]);
+		} while(x[i]<0 || x[i]>max);
+	}
+	
+	counting(x,nx,min,max);
+	puts("오름차순으로 정렬했습니다.");
+	
+	for(i=0;i<nx;i++)
+		printf("x[%d] = %d\n",i,x[i]);
+	
+	free(x);
+	
+	return 0;
+}
