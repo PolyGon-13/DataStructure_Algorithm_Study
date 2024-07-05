66f97ceb1f0e10417bdfcb78b11aa530b2408806
Donghyun Kim <hyundiego@hanyang.ac.kr>
2024-07-05 22:09:32 +0900

    Update 7-7_str_len2.c

--- C/practice/07/7-7_str_len2.c
+++ C/practice/07/7-7_str_len2.c
@@ -1,7 +1,8 @@
+// Ver1.7-6
 #include <stdio.h>
 #include <string.h>
 
-int str_len(const char *s)
+size_t str_len(const char *s)
 {
 	int len=0;
 	
@@ -16,7 +17,38 @@ int main()
 	char str[256];
 	printf("문자열 : ");
 	scanf("%s",str);
-	printf("이 문자열의 길이는 %d입니다.\n",str_len(str));
+	printf("이 문자열의 길이는 %ld입니다.\n",str_len(str));
 	
 	return 0;
 }
+
+// Ver2.7-7
+/*
+#include <stdio.h>
+#include <string.h>
+
+size_t str_len(const char *s)
+{
+	size_t len=0;
+	
+	while(*s++)
+		len++;
+	
+	return len;
+}
+
+int main()
+{
+	char str[256];
+	printf("문자열 : ");
+	scanf("%s",str);
+	printf("이 문자열의 길이는 %ld입니다.\n",str_len(str));
+	
+	return 0;
+}
+*/
+
+// Ver3.7-8
+/*
+
+*/
