da7460d09f50c915621ab613fecbf9f5978e080a
Donghyun Kim <hyundiego@hanyang.ac.kr>
2024-07-05 22:10:09 +0900

    Update Q-4.c

--- C/question/07/Q-4.c
+++ C/question/07/Q-4.c
@@ -1,11 +1,12 @@
+// Ver1.7-6
 #include <stdio.h>
 #include <string.h>
 
-int str_len(const char *s)
+size_t str_len(const char *s)
 {
-	size_t len=0;
+	int len=0;
 	
-	while(s[len])
+	while(*s++)
 		len++;
 	
 	return len;
@@ -16,7 +17,38 @@ int main()
 	char str[256];
 	printf("문자열 : ");
 	scanf("%s",str);
-	printf("이 문자열의 길이는 %d입니다.\n",str_len(str));
+	printf("이 문자열의 길이는 %ld입니다.\n",str_len(str));
 	
 	return 0;
 }
+
+// Ver2.7-7
+/*
+#include <stdio.h>
+#include <string.h>
+
+size_t str_len(const char *s)
+{
+	size_t len=0;
+	
+	while(*s++)
+		len++;
+	
+	return len;
+}
+
+int main()
+{
+	char str[256];
+	printf("문자열 : ");
+	scanf("%s",str);
+	printf("이 문자열의 길이는 %ld입니다.\n",str_len(str));
+	
+	return 0;
+}
+*/
+
+// Ver3.7-8
+/*
+
+*/
