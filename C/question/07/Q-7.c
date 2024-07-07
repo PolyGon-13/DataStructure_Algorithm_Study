d4c956783492b0fb7d537fb7453cd27dfde01283
Donghyun Kim <hyundiego@hanyang.ac.kr>
2024-07-07 22:44:19 +0900

    Create Q-7.c

---
+++ C/question/07/Q-7.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <string.h>
+
+int str_ncmp(const char *s1, const char *s2, size_t n)
+{
+    for(size_t i=0;i<n;++i)
+        if(s1[i]!=s2[i] || s1[i]=='\0' || s2[i]=='\0')
+            return (unsigned char)s1[i]-(unsigned char)s2[i];
+	
+    return 0;
+}
+
+int main()
+{
+    char st[128];
+    puts("\"STRING\"의 처음 3개의 문자와 비교합니다.");
+    puts("\"XXXX\"를 입력하면 종료합니다.");
+    
+    while(1)
+    {
+        printf("문자열 st : ");
+        scanf("%s", st);
+        if (str_ncmp("XXXX", st, 3) == 0)
+            break;
+        printf("str_ncmp(\"STRING\", st, 3) = %d\n", str_ncmp("STRING", st, 3));
+    }
+    
+    return 0;
+}
