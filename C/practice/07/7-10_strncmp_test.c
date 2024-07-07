b457e681d90c3d70ddd59e0c1579782323dea079
Donghyun Kim <hyundiego@hanyang.ac.kr>
2024-07-07 22:38:54 +0900

    Create 7-10_strncmp_test.c

---
+++ C/practice/07/7-10_strncmp_test.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+	char st[128];
+	puts("\"STRING\"의 처음 3개의 문자와 비교합니다.");
+	puts("\"XXXX\"를 입력하면 종료합니다.");
+	
+	while(1)
+	{
+		printf("문자열 st : ");
+		scanf("%s",st);
+		if(strncmp("XXXX",st,3)==0)
+			break;
+		printf("strncmp(\"STRING\",st,3) =%d\n",strncmp("STRING",st,3));
+	}
+	
+	return 0;
+}
