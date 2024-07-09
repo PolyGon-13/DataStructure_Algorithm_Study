996ca34e18c4fedcd67bffdad103309279173b48
Donghyun Kim <hyundiego@hanyang.ac.kr>
2024-07-09 22:47:26 +0900

    Update 7-13_kmp_match.c

--- C/practice/07/7-13_kmp_match.c
+++ C/practice/07/7-13_kmp_match.c
@@ -2,32 +2,33 @@
 
 int kmp_match(const char txt[],const char pat[])
 {
-	int pt=1;
+	int pt=1; // kmp법에서 패턴끼리 비교하여 다시 시작값의 표를 작성하기 위해 시작포인트를 1번 인덱스로 지정
 	int pp=0;
-	int skip[1024];
+	int skip[1024]; // 다시 시작값의 표를 저장할 배열
 	
 	skip[pt]=0;
 	while(pat[pt]!='\0')
 	{
-		if(pat[pt]==pat[pp])
+		if(pat[pt]==pat[pp]) // 패턴끼리 일치하는 부분 발견
 			skip[++pt]=++pp;
-		else if(pp==0)
+		else if(pp==0) // 패턴끼리 일치X && pp가 0인 경우
 			skip[++pt]=pp;
-		else
-			pp=skip[pp];
+		else // 패턴끼리 일치X && pp가 0이 아닌 경우
+			pp=skip[pp]; // 이전에 일치했던 부분 제외
 	}
 	
 	pt=pp=0;
 	while(txt[pt]!='\0' && pat[pp]!='\0')
 	{
-		if(txt[pt]==pat[pp])
+		if(txt[pt]==pat[pp]) // 문자끼리 일치하는 경우
 		{
 			pt++;
 			pp++;
+			// 텍스트와 패턴 1칸 이동하여 다음 문자를 계속해서 비교
 		}
-		else if(pp==0)
-			pt++;
-		else
+		else if(pp==0) // 문자끼리 일치X && pp가 0인 경우
+			pt++; // 텍스트 1칸 이동
+		else // 문자끼리 일치X && pp가 0이 아닌 경우
 			pp=skip[pp];
 	}
 	
