70aba0adeb39e9eb367f748b49b4fc5be493792b
Donghyun Kim <hyundiego@hanyang.ac.kr>
2024-07-10 21:54:57 +0900

    Update 7-15_strstr_test.c

--- C/practice/07/7-15_strstr_test.c
+++ C/practice/07/7-15_strstr_test.c
@@ -15,7 +15,7 @@ int main()
 		printf("텍스트에 패턴이 없습니다.\n");
 	else
 	{
-		int ofs=p-s1;
+		int ofs=p-s1; // 찾은 문자에 대한 포인터에서 s1배열의 시작점을 가리키는 포인터를 빼면, 두 포인터 간의 거리를 계산할 수 있음
 		printf("\n%s\n",s1);
 		printf("%*s|\n",ofs,"");
 		printf("%*s%s\n",ofs,"",s2);
