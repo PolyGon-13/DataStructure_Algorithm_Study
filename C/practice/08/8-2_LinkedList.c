97649d47dab62709244f2e08d5c680980b58f1a8
Donghyun Kim <hyundiego@hanyang.ac.kr>
2024-07-11 22:19:07 +0900

    Update 8-2_LinkedList.c

--- C/practice/08/8-2_LinkedList.c
+++ C/practice/08/8-2_LinkedList.c
@@ -59,3 +59,95 @@ void InsetRear(List *list,const Member *x)
 		SetNode(ptr->next,x,NULL);
 	}
 }
+
+void RemoveFront(List *list)
+{
+	if(list->head != NULL)
+	{
+		Node *ptr=list->head->next;
+		free(list->head);
+		list->head=list->crnt=ptr;
+	}
+}
+
+void RemoveRear(List *list)
+{
+	if((list->head)->next==NULL)
+	{
+		if((list->head)->next==NULL)
+			RemoveFront(list);
+		else
+		{
+			Node *ptr=list->head;
+			Node *pre;
+			while(ptr->next!=NULL)
+			{
+				pre=ptr;
+				ptr=ptr->next;
+			}
+			pre->next=NULL;
+			free(ptr);
+			list->crnt=pre;
+		}
+	}
+}
+
+void RemoveCurrent(List *list)
+{
+	if(list->head!=NULL)
+	{
+		if(list->crnt==list->head)
+			RemoveFront(list);
+		else
+		{
+			Node *ptr=list->head;
+			while(ptr->next!=list->crnt)
+				ptr=ptr->next;
+			ptr->next=list->crnt->next;
+			free(list->crnt);
+			list->crnt=ptr;
+		}
+	}
+}
+
+void Clear(List *list)
+{
+	while(list->head!=NULL)
+		RemoveFront(list);
+	list->crnt=NULL;
+}
+
+void PrintCurrent(const List *list)
+{
+	if(list->crnt==NULL)
+		printf("선택한 노드가 없습니다.");
+	else
+		PrintMember(&list->crnt->data);
+}
+
+void PrintLnCurrent(const List *list)
+{
+	PrintCurrent(list);
+	putchar('\n');
+}
+
+void Print(const List *list)
+{
+	if(list->head==NULL)
+		puts("노드가 없습니다.");
+	else
+	{
+		Node *ptr=list->head;
+		puts("[모두 보기]");
+		while(ptr!=NULL)
+		{
+			PrintLnMember(&ptr->data);
+			ptr=ptr->next;
+		}
+	}
+}
+
+void Terminate(List *list)
+{
+	Clear(list);
+}
