dd25f3fb460bd92c6982d2a987ec1d5faff77199
Donghyun Kim <hyundiego@hanyang.ac.kr>
2024-07-15 23:52:19 +0900

    Update ArrayLinkedList.h

--- C/question/08/Q-3/ArrayLinkedList.h
+++ C/question/08/Q-3/ArrayLinkedList.h
@@ -9,18 +9,18 @@ typedef int Index;
 
 typedef struct
 {
-	Member data; // 데이터
-	Index next; // 다음 노드
-	Index Dnext; // 프리 리스트의 다음 노드
+	Member data;
+	Index next;
+	Index Dnext;
 } Node;
 
 typedef struct
 {
-	Node *n; // 리스트 본체
-	Index head; // 머리 노드
-	Index max; // 사용 중인 꼬리 레코드
-	Index deleted; // 프리 리스트의 머리 커서
-	Index crnt; // 선택한 노드
+	Node *n;
+	Index head;
+	Index max;
+	Index deleted;
+	Index crnt;
 } List;
 
 void Initialize(List *list,int size);
