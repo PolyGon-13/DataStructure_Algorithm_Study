294cbdfda935f5ae42b9d6c01cc8d513ecf62a69
Donghyun Kim <hyundiego@hanyang.ac.kr>
2024-07-15 23:21:09 +0900

    Update Q-1.c

--- C/question/08/Q-1.c
+++ C/question/08/Q-1.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include "Member.h"
-#include "LinkedList.h"
+#include "ArrayLinkedList.h"
 
 typedef enum
 {
@@ -17,7 +17,7 @@ Menu SelectMenu(void)
 		"머리에 노드를 삽입","꼬리에 노드를 삽입","머리 노드를 삭제",
 		"꼬리 노드를 삭제","선택한 노드를 출력","선택한 노드를 삭제",
 		"번호로 검색","이름으로 검색","모든 노드를 출력",
-		"모든 노드를 삭제"
+		"모든 노드를 삭제",
 	};
 	
 	do
@@ -35,34 +35,12 @@ Menu SelectMenu(void)
 	return (Menu)ch;
 }
 
-void Purge(List *list,int compare(const Member *x,const Member *y))
-{
-	Node *ptr=list->head;
-	Node *next_ptr;
-	
-	while(ptr!=NULL)
-	{
-		next_ptr=ptr->next;
-		
-		while(next_ptr!=NULL)
-		{
-			if(compare(&ptr->data,&next_ptr->data)==0) // ptr이 가리키는 데이터와 next_ptr이 가리키는 데이터가 같은 경우
-			{
-				list->crnt=next_ptr; // 현재 커서를 중복되는 데이터로 옮기기
-				RemoveCurrent(list); // 해당 데이터 제거
-			}
-			next_ptr=next_ptr->next; // NULL이 되어버린 노드를 버리고 그 다음 노드를 가리키게 함
-		}
-		ptr=ptr->next; // NULL이 되어버린 노드의 앞 노드는 next_ptr이 가리키게 된 새로운 노드를 가리키게 함
-	}
-	list->crnt=list->head;
-}
-
 int main()
 {
 	Menu menu;
 	List list;
-	Initialize(&list);
+	Initialize(&list,30);
+	
 	do
 	{
 		Member x;
@@ -90,14 +68,14 @@ int main()
 				break;
 			case SRCH_NO:
 				x=ScanMember("검색",MEMBER_NO);
-				if(Search(&list,&x,MemberNoCmp)!=NULL)
+				if(search(&list,&x,MemberNoCmp)!=Null)
 					PrintLnCurrent(&list);
 				else
 					puts("그 번호의 데이터가 없습니다.");
 				break;
 			case SRCH_NAME:
 				x=ScanMember("검색",MEMBER_NAME);
-				if(Search(&list,&x,MemberNameCmp)!=NULL)
+				if(search(&list,&x,MemberNameCmp)!=Null)
 					PrintLnCurrent(&list);
 				else
 					puts("그 이름의 데이터가 없습니다.");
@@ -110,37 +88,8 @@ int main()
 				break;
 		}
 	} while(menu!=TERMINATE);
-	Purge(&list,MemberNameCmp);
-	Purge(&list,MemberNoCmp);
 	
 	Terminate(&list);
 	
 	return 0;
 }
-
-/*
-void Purge(List *list,int compare(const Member *x,const Member *y))
-{
-	Node *ptr=list->head;
-	
-	while(ptr!=NULL)
-	{
-		Node *ptr2=ptr;
-		Node *pre=ptr;
-		
-		while(pre->next!=NULL)
-		{
-			ptr2=pre->next;
-			if(!compare(&ptr->data,&ptr2->data))
-			{
-				pre->next=ptr2->next;
-				free(ptr2);
-			}
-			else
-				pre=ptr2;
-		}
-		ptr=ptr->next;
-	}
-	list->crnt=list->head;
-}
-*/
