4fa161e86f94f24755fcf6558fd7b2954d8273f2
Donghyun Kim <hyundiego@hanyang.ac.kr>
2024-07-20 22:54:16 +0900

    Create 10-4_ChainHash.c

---
+++ C/practice/10/10-4_ChainHash.c
@@ -0,0 +1,66 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "Member.h"
+#include "ChainHash.h"
+
+static int hash(int key,int size)
+{
+    return key%size;
+}
+
+static void SetNode(Node *n,const Member *x,const Node *next)
+{
+    n->data=*x;
+    n->next=next;
+}
+
+int Initialize(ChainHash *h,int size)
+{
+    if((h->table=calloc(size,sizeof(Node *)))==NULL)
+    {
+        h->size=0;
+        return 0;
+    }
+
+    int i;
+    h->size=size;
+    for(i=0;i<size;i++)
+        h->table[i]=NULL;
+
+    return 1;
+}
+
+Node *Search(const ChainHash *h,const Member *x)
+{
+    int key=hash(x->no,h->size);
+    Node *p=h->table[key];
+    
+    while(p!=NULL)
+    {
+        if(p->data.no==x->no)
+            return p;
+        p=p->next;
+    }
+
+    return NULL;
+}
+
+int Add(ChainHash *h,const Member *x)
+{
+    int key=hash(x->no,h->size);
+    Node *p=h->table[key];
+    Node *temp;
+    
+    while(p!=NULL)
+    {
+        if(p->data.no==x->no)
+            return 1;
+        p=p->next;
+    }
+
+    if((temp=calloc(1,sizeof(Node)))==NULL)
+        return 2;
+    SetNode(temp,x,h->table[key]);
+    h->table[key]=temp;
+    return 0;
+}
