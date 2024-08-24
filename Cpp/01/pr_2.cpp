f80fa81f8bb1ee3c15811998f1c75fcd0197fea1
Donghyun Kim <hyundiego@hanyang.ac.kr>
2024-08-24 23:54:02 +0900

    Update pr_2.cpp

--- Cpp/01/pr_2.cpp
+++ Cpp/01/pr_2.cpp
@@ -10,6 +10,10 @@ auto build_array(Args&&... args) -> array<typename common_type <Args...>::type, 
 	using commonType=typename common_type<Args...>::type;
 	return {forward<commonType>((Args&&)args)...};
 }
+// common_type은 여러 자료형들 중에서 가장 일반적인 자료형을 결정 (int와 float가 있으면 float를 반환)
+// common_type은 트레이트 자료형으로 type 멤버를 통해 결과 자료형을 제공함
+// sizeof...(args)는 파라미터 팩(args)의 요소 개수를 반환
+
 
 int main()
 {
