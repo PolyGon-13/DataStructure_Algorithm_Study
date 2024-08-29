a288eddd6839174ad3b592d4d4667606f3b91eb0
Donghyun Kim <hyundiego@hanyang.ac.kr>
2024-08-29 22:59:18 +0900

    Update pr_4.cpp

--- Cpp/01/pr_4.cpp
+++ Cpp/01/pr_4.cpp
@@ -28,10 +28,12 @@ int main()
 	forward_list<string> fwd(vec.begin(),vec.end());
 	
 	auto it1=fwd.begin();
-	cout<<"가장 최근 우승자 : "<<*it<<endl;
+	cout<<"가장 최근 우승자 : "<<*it1<<endl;
 	
-	advance(it1,5);
-	cout<<"5년 전 우승자 : "<<*it<<endl;
+	advance(it,5);
+	// it+=5; forward_list는 임의 접근 반복자가 아니기 때문에 해당 코드는 에러 발생
+	// it++; 를 여러번 사용하여 하나씩 이동은 가능
+	cout<<"5년 전 우승자 : "<<*it1<<endl;
 	
 	// advance(it1,-2); forward_list는 순방향으로만 이동할 수 있기에 해당 코드는 에러 발생
 	
