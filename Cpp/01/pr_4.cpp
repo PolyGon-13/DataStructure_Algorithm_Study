b168f0857a8db7c09f822e564a145a42beb98705
Donghyun Kim <hyundiego@hanyang.ac.kr>
2024-10-21 21:55:59 +0900

    Update pr_4.cpp

--- Cpp/01/pr_4.cpp
+++ Cpp/01/pr_4.cpp
@@ -30,7 +30,7 @@ int main()
 	auto it1=fwd.begin();
 	cout<<"가장 최근 우승자 : "<<*it1<<endl;
 	
-	advance(it,5);
+	advance(it1,5);
 	// it+=5; forward_list는 임의 접근 반복자가 아니기 때문에 해당 코드는 에러 발생
 	// it++; 를 여러번 사용하여 하나씩 이동은 가능
 	cout<<"5년 전 우승자 : "<<*it1<<endl;
